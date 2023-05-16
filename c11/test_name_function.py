38aa7fa5efaee057defda82c5646811edc81cd79
Mario Uriarte <mario.uriarte@gmail.com>
2023-05-16 10:39:41 -0400

    Class Bullet implemented

--- c11/test_name_function.py
+++ c11/test_name_function.py
@@ -1,7 +1,7 @@
 from name_function import get_formatted_name
 
 def test_first_last_name():
-    '''Do namesj like Janis Jopling'''
+    '''Do names like Janis Jopling'''
     formatted_name = get_formatted_name('janis', 'arturo', 'joplin')
 
     assert formatted_name == 'Janis Arturo Joplin'
