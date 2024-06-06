54c3136e8e24af2a4c6a65b2d778adb920977748
Mario Uriarte <mario.uriarte@gmail.com>
2024-06-05 23:59:16 -0400

    install matplotlib

--- c11/name.py
+++ c11/name.py
@@ -1,17 +1,13 @@
 from name_function import get_formatted_name
 
 print("Enter 'q' at any time to quit.")
-
 while True:
     first = input("\nPlease give me a first name: ")
-
     if first == 'q':
         break
-
     middle = input("Please give me a middle name: ")
     if middle == 'q':
         break
-
     last = input("Please give me a last name: ")
     if last == 'q':
         break
--- c11/test_name_function.py
+++ c11/test_name_function.py
@@ -5,3 +5,9 @@ def test_first_last_name():
     formatted_name = get_formatted_name('janis', 'arturo', 'joplin')
 
     assert formatted_name == 'Janis Arturo Joplin'
+
+def test_first_last_name_no_deberian_ser_en_minusculas():
+    '''Do names like Janis Jopling'''
+    formatted_name = get_formatted_name('janis', 'arturo', 'joplin')
+
+    assert formatted_name != 'janis arturo joplin'
