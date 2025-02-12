9903c1a37803dddc3710ddabbccc7cbc37eecc4b
Mario Uriarte <mario.uriarte@gmail.com>
2025-02-12 14:29:47 -0400

    Dev

--- c18/learning_log/learning_logs/models.py
+++ c18/learning_log/learning_logs/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
-    '''A topic the user is learning aboud.'''
+    """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        '''Retrun a string representation of the model.'''
+        """Return a string representation of the model."""
         return self.text
 
 class Entry(models.Model):
-    '''Something specific learned about a topic.'''
+    """Something specific learned about a topic."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
